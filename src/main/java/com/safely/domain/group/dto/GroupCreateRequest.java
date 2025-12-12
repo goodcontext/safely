1fb6edbc5548ec434d763b37d3db288d27e6a7d0
goodcontext <goodcontext@naver.com>
2025-12-12 11:53:01 +0900

    fix: GroupCreateRequest DTO @NotBlank -> @NotNull로 변경

--- src/main/java/com/safely/domain/group/dto/GroupCreateRequest.java
+++ src/main/java/com/safely/domain/group/dto/GroupCreateRequest.java
@@ -2,6 +2,7 @@ package com.safely.domain.group.dto;
 
 import com.safely.domain.group.entity.Group;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,10 +18,10 @@ public class GroupCreateRequest {
     @NotBlank(message = "그룹 이름은 필수입니다.")
     private String name;
 
-    @NotBlank(message = "여행 시작일은 필수입니다.")
+    @NotNull(message = "여행 시작일은 필수입니다.")
     private LocalDate startDate;
 
-    @NotBlank(message = "여행 종료일은 필수입니다.")
+    @NotNull(message = "여행 종료일은 필수입니다.")
     private LocalDate endDate;
 
     private String destination;
