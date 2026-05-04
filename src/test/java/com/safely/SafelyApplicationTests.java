32e18d676ee069b36a60dceecffb2ca4208c883c
goodcontext <goodcontext@naver.com>
2026-05-05 02:02:27 +0900

    fix: CI/CD 오류 (S3 bucket 이름 등이 예전 계정으로 된 오류) 및 테스트 오류 뜨는 거 수정

--- src/test/java/com/safely/SafelyApplicationTests.java
+++ src/test/java/com/safely/SafelyApplicationTests.java
@@ -1,7 +1,9 @@
 package com.safely;
 
+import com.safely.global.s3.S3Service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class SafelyApplicationTests {
